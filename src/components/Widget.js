8c9ff2b82b2d28e26d79fe892ef0865d0d7370e1
unknown <kgeluza@mail.ru>
2022-12-23 16:39:52 +0400

    commit 23/12

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,76 +1,55 @@
-import React, {useState, useEffect} from 'react';
+import React from "react";
+import Note from "./Note";
+import AddForm from "./AddForm";
 
+const server = 'http://localhost:7777/notes';
 
+export default class Widget extends React.Component {
+    state = {notes: []};
 
-export default function Widget() {
- let [notes, setNotes] = useState([]);
- let [form, setForm] = useState('');
- 
- const requestUrl = 'http://localhost:7777/notes';
- const headers = {
-	'Content-Type': 'application/json'
-}
+    getNotes = () => {
+        fetch(server).then(response => response.json())
+        .then(result => {
+            this.setState({notes: [...result]});
+        });
+    }
 
- const handleChange = (evt) => {
-   setForm(evt.target.value)
- }
+    postNote = (id, content) => {
+        fetch(server, {
+            method: 'POST',
+            body: JSON.stringify({id, content}),
+        }).then(this.getNotes);
+    }
 
- const handleRemove = async id => {
-  const url = requestUrl + `/${id}`;
-  await fetch(url, {method: "DELETE"});
-  await update();
- }
+    deleteNote = (id) => {
+        fetch(`${server}/${id}`, {
+            method: 'DELETE',
+        }).then(this.getNotes);
+    }
 
- const update = async () => {
-  const data = await fetch(requestUrl, { method: 'GET' }).then(response => {
-    return response.json();
-  });
-  setNotes(() => {
-    return data;
-  })
- }
+    componentDidMount() {
+        this.getNotes();
+    }
 
- const handleSubmit = async evt => {
-  evt.preventDefault();
-  if (!form) return;
-  const body = {
-    "id": 0,
-    "content": form.story
-  }
-  await fetch(requestUrl, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: headers
-  });
-  setForm('');
-  await update();
-};
-
-  useEffect(() => {
-  update()
-});
-
-  return (
-    <div className="widget_crud">
-    <div className="notes">
-    <h2 className="notes_title">Notes</h2>
-    <button className="update" onClick={() => update()}>Update</button>
-    {notes.map((elm) => {
-     return(
-      <div className="note_content">
-      <div className="note_text">{elm.content}</div>
-      <button className="btn_delete_note" onClick={handleRemove(elm.id)}>x</button>
-      </div>
-     )
-    })}
-    </div>
-    <form className="add_note_form" submit={handleSubmit}>
-      <h3 className="title_form">New Note</h3>
-        <textarea className="textarea_note" onChange={handleChange} value={form}></textarea>
-        <button className="add_button">Add</button>
-    </form>
-    </div>
-  );
-  
+    render() {
+        return (
+            <div className='Widget'>
+                <div className='header'>
+                   <h2 className='title'>Notes</h2>
+                   <button className='update' onClick={this.getNotes}>reload</button> 
+                </div>
+                <div className='notes'>
+                    {this.state.notes.map((el) => {
+                        console.log(el.content);
+                        return (
+                            <Note content={el.content} key={el.id}>
+                                <button className='delete_btn' onClick={() => this.deleteNote(el.id)}>X</button>
+                            </Note>)
+                    })}
+                </div>
+                <AddForm newNote={this.postNote} />
+            </div>
+        )
+    }
 }
 
